ba5c1e3295b478c9291478360dbb77787a4c84fd
MohammedHsin <mohammedhsin.technological@gmail.com>
2023-03-14 18:19:05 +0300

    set up some stuff

--- app/src/main/java/com/example/stocky/MainActivity.kt
+++ app/src/main/java/com/example/stocky/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     
-                    DestinationsNavHost(navGraph = NavGraphs.root)
+//                    DestinationsNavHost(navGraph = NavGraphs.root)
 
                 }
             }

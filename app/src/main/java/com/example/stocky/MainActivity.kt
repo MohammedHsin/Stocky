f2e95fd7ff63743ac823b349e1ebf16e482d7155
MohammedHsin <mohammedhsin.technological@gmail.com>
2023-03-17 15:31:08 +0300

    stable , after rolling back the stable version of android studio

--- app/src/main/java/com/example/stocky/MainActivity.kt
+++ app/src/main/java/com/example/stocky/MainActivity.kt
@@ -27,9 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    
-//                    DestinationsNavHost(navGraph = NavGraphs.root)
-
+//                    DestinationsNavHost(navGraph = N)
                 }
             }
         }
